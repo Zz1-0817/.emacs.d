4e9b345f02d1bbd6e3f54832f27d62c888f7cb07
Zz1au <1626741109lzz@gmail.com>
2026-08-08 20:45:37 +0800

    Add completion plugin configurations

--- init.el
+++ init.el
@@ -1,9 +1,46 @@
+(setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
+(load custom-file 'noerror)
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (electric-pair-mode t)
+(which-key-mode 1)
+(add-hook 'after-init-hook 'global-display-line-numbers-mode)
+(setq display-line-numbers-type 'relative)
+(setq make-backup-files nil)
 
-(use-package company
-  :ensure t               
-  :init
-  (global-company-mode t))
+(use-package vertico
+  :ensure t
+  :init (vertico-mode 1))
+
+(use-package marginalia
+  :ensure t
+  :after vertico
+  :config (marginalia-mode 1))
+
+(use-package orderless
+  :ensure t
+  :config
+  (setq completion-styles '(orderless)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles basic partial-completion)))))
+
+(use-package corfu
+  :ensure t
+  :config
+  (global-corfu-mode 1)
+  (setq corfu-auto t
+	corfu-quit-at-boundary t))
+
+(use-package cape
+  :ensure t
+  :config
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  (add-to-list 'completion-at-point-functions #'cape-elisp-symbol)
+  (add-to-list 'completion-at-point-functions #'cape-keyword))
+
+(use-package rust-ts-mode
+  :ensure t
+  :mode "\\.rs\\'"
+  )
